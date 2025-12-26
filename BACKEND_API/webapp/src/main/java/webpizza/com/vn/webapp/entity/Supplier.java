d2eb6298e9ab87410ab92307c303e88e07a953c1
Tran Phuc <cantho1995ct@gmail.com>
2025-12-26 09:36:44 +0700

    luu thay doi 26/12/2025

---
+++ BACKEND_API/webapp/src/main/java/webpizza/com/vn/webapp/entity/Supplier.java
@@ -0,0 +1,16 @@
+// package webpizza.com.vn.webapp.entity;
+// import jakarta.persistence.Table;
+// import lombok.Data;
+
+
+// @Data
+// @Table(name = "suppliers")
+// public class Supplier{
+  
+//     @Id
+//     @GeneratedValue(stategy = GenerationType.IDENTITY)
+//     private int id;
+
+    
+    
+// }
